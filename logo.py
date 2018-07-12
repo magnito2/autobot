65291b6c4920e7da36657da1cb56dabc2cae65cc
Magnito2 <magnusotwani@gmail.com>
2018-07-12 11:33:38 +0300

    cleaned up

--- logo.py
+++
@@ -1,5 +0,0 @@
-
-def print_logo():
-    f = open('logo.txt','r')
-    for line in f.readlines():
-        print(line)
--- settings.py
+++
@@ -1,9 +0,0 @@
-
-'''
-######### Account Settings ############
-'''
-API_KEY = "U4xdzDde4499dqL8JfBs53Ybx8004rbcvBddiSOZpjOP61Z70Po5AbX3Cmaok1XP"
-API_SECRET = "dF0x7vhqKZe9XM7B8EJx377ZNXAEZ3WVKqXY3GKCT325ZXnHrJiG6594io16psyV"
-
-#dont touch
-time_frames_intervals = ['1m','3m','5m','15m','30m','1h','2h','4h','6h','8h','12h','1d','3d','1w','1M']
