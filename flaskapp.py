1a20b36410787f8fd61be02a16c058141f3023f3
Magnito2 <magnusotwani@gmail.com>
2018-08-10 06:25:25 +0300

    cleanup

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,10 @@
 from dashboard.app import app, db, socketio
-from dashboard.app.models import User, Bot, Role, Payment, Log
+from dashboard.app.models import User, Bot, Role, Payment, Log, Feedback
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Bot': Bot, 'Role':Role, 'Payment':Payment, 'Log': Log}
+    return {'db': db, 'User': User, 'Bot': Bot, 'Role':Role, 'Payment':Payment, 'Log': Log, 'Feedback': Feedback}
 
 
 if __name__ == "__main__":
