be02dab7df476318749d97e9f5b98c9616c72487
Magnito2 <magnusotwani@gmail.com>
2018-08-30 22:28:20 +0300

    get logging working again

--- main.py
+++ main.py
@@ -20,4 +20,9 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
 websocket_manager.manage(socketio_handler)
